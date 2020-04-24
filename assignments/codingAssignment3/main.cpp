7be10bfbbc8382aba3adcefe3badf6234e26e0d5
DiegoELT99 <diegoelt99@gmail.com>
2020-04-24 13:53:10 -0500

    Improvements in third assignment, and first lab.

--- assignments/codingAssignment3/main.cpp
+++ assignments/codingAssignment3/main.cpp
@@ -4,13 +4,15 @@
 extern int yylex();
 extern int yylineno;
 extern char* yytext;
+extern FILE* yyin;
 
 std::string types[] = {"null", "KEYWORD", "OPERATOR", "SPECIAL_SYMBOL", "IDENTIFIER", "INT_CONSTANT", "FLOAT_CONSTANT", "UNEXPECTED_CHAR"};
 
 std::ofstream tokenTable("tokenTable.txt");
 
-int main()
+int main(int argc, char* argv[])
 {
+    yyin = fopen(argv[1],"r");
     int tokenCategory;
     tokenCategory = yylex();
     while (tokenCategory)
